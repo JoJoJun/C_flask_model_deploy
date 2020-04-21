2e24f9518d521b89217ca2dfbb8ddc3e7dfda872
cxy <474622332@qq.com>
2020-04-21 18:18:37 +0800

    首页

--- project/routes/project_view.py
+++ project/routes/project_view.py
@@ -1,6 +1,7 @@
 from project.models.model import User,Project
 from flask import Flask, redirect, url_for,render_template,request,Blueprint
-from project.services.project_service import list_all_project, check_project_same_name
+
+from project.services.project_service import list_all_project, check_project_same_name,goDeletePro
 import flask_login
 import datetime
 from project.models import db
@@ -24,6 +25,7 @@ def addPro():
    name = request.form['name']
    url = request.form['url']
    des = request.form['description']
+   print(name,url,des)
    user_account = flask_login.current_user.account
    print(name, url, des)
    # pro = Project.query.filter_by(name=name,user=user_account).first()
@@ -45,4 +47,52 @@ def addPro():
    # res['code']=code
    # res['msg']=msg
    # return render_template('index.html',user=flask_login.current_user,res=res)
+
+
+
+@project_bp.route('/editPro/', methods=['GET', 'POST'])#编辑项目
+def editPro():
+   if request.method == 'GET':
+      return render_template('create_project.html',user = flask_login.current_user)
+   id = request.form['project_id']
+   name = request.form['Name']
+   url = request.form['url']
+   des = request.form['description']
+   res = {}
+   user_account = flask_login.current_user.account
+   if check_project_same_name(name, user_account):
+       code = 2004
+       msg = '用户名下已存在同名项目'
+       res['code'] = code
+       res['msg'] = msg
+       return render_template('index.html', user=flask_login.current_user, res=res)
+   else:
+       pro = Project.query.filter_by(id=id).first()
+       pro.name = name
+       pro.url = url
+       pro.description = des
+       db.session.commit()
+       res['code'] = 1000
+       res['msg'] = '操作成功'
+       return render_template('index.html', user=flask_login.current_user, res=res)
+
+@project_bp.route('/deletePro/',methods = ['POST', 'GET'])#删除项目
+def deletePro():
+    pid = request.form.get("id")
+    print('test'+pid)
+    flag = goDeletePro(pid)
+    res = {}
+
+
+    if flag:  # 删除成功
+        res['code'] = 1000
+        res['msg'] = '删除成功'
+        return render_template('index.html', user=flask_login.current_user, res=res)
+    else:
+        res['code'] = 1004
+        res['msg'] = '删除失败'
+        return render_template('index.html', user=flask_login.current_user, res=res)
+
+
+
 
--- project/services/project_service.py
+++ project/services/project_service.py
@@ -1,6 +1,8 @@
-from project.models.model import Project,User
+from project.models.model import Project,User,SQLAlchemy
 from flask_login import current_user
+from project.models import db
 from datetime import datetime
+
 def list_all_project():
     # user = User.query.filter_by(account = current_user).first()
     list = Project.query.filter_by(state=0,user=current_user.account).all()
@@ -19,3 +21,13 @@ def list_all_project():
 # user name的project是否已存在
 def check_project_same_name(name,user_account):
     return Project.query.filter_by(name=name, user=user_account).first()
+
+def goDeletePro(pid):
+    pro = db.session.query(Project).filter_by(id=pid).first()
+    db.session.delete(pro)
+    db.session.commit()
+
+    if Project.query.filter_by(id=pid).first():
+        return False
+    else:
+        return True
