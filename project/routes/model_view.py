c07c81de9842076b763718b1dd973b603980a136
cxy <474622332@qq.com>
2020-04-24 17:48:38 +0800

    修改删除项目的一点问题

--- project/routes/model_view.py
+++ project/routes/model_view.py
@@ -1,4 +1,13 @@
 from project.models.model import User
 from flask import Flask, redirect, url_for,render_template,request,Blueprint
 ## 登录的路由和逻辑都在这页了
+import flask_login
+import datetime
+from project.services.model_service import list_all_model
+from project.models import db
 model_bp = Blueprint('model', __name__, url_prefix='/model')
+# 登录后的全部项目列表页
+@model_bp.route('/view/<project_id>', methods=['GET', 'POST'])
+def model_view(project_id):
+   list = list_all_model(project_id)
+   return render_template('model.html', user=flask_login.current_user, data=list)
