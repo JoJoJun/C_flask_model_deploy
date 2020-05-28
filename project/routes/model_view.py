915dc2a69488d30f0887c5d7ee6c4017dbcd3257
cxy <474622332@qq.com>
2020-05-28 14:19:44 +0800

    内存

--- project/routes/model_view.py
+++ project/routes/model_view.py
@@ -224,8 +224,8 @@ def editParam(model_id):
                 memory = request.form['memory']
 
                 if int(memory) > 1024:
-                    res['code'] = 2000
-                    res['msg'] = '服务器错误，请检查参数'
+                    res['code'] = 2021
+                    res['msg'] = '内存超过限制大小'
                     return jsonify(res)
             else:
                 memory = 0
@@ -235,8 +235,8 @@ def editParam(model_id):
             if mem == 'true':
                 memory = request.form['memory']
                 if int(memory) > 1024:
-                    res['code'] = 2000
-                    res['msg'] = '服务器错误，请检查参数'
+                    res['code'] = 2021
+                    res['msg'] = '内存超过限制大小'
                     return jsonify(res)
             else:
                 memory = 0
